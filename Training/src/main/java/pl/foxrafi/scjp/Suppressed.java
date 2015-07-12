803a8008c19d9140fc8976e2d1fc7af8652ecc3b
foxrafi <foxrafi@gmail.com>
2015-07-12 22:57:36 +0200

    test

--- Training/src/main/java/pl/foxrafi/scjp/Suppressed.java
+++ Training/src/main/java/pl/foxrafi/scjp/Suppressed.java
@@ -5,14 +5,14 @@ public class Suppressed {
 	//private static Logger logger = LogManager.getLogger(Suppressed.class);
 
 	public static void main(String[] args) {
-		try (One one = new One()) {
+		/*try (One one = new One()) {
 			throw new Exception("Try");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			for (Throwable t : e.getSuppressed()) {
 				System.err.println("suppressed:" + t);
 			}
-		}
+		}*/
 	}
 
 }
