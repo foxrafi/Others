a0a14f328c512d658008c23bee1f872e68c8c0f9
foxrafi <foxrafi@gmail.com>
2015-07-12 22:54:20 +0200

    redundant folders were deleted

--- JodaArtifactId/src/main/java/pl/foxrafi/joda/tests/IntUtils/Test2_IntUtils.java
+++
@@ -1,9 +0,0 @@
-package pl.foxrafi.joda.tests.IntUtils;
-
-public class Test2_IntUtils {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- human/sample.js
+++
@@ -1,3 +0,0 @@
- if(window.location.href.indexOf("/login") > -1){
-    window.scrollTo(0, 0);
-  }
